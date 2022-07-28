fb9229a06d4fcecbff0378737051992a2d88f985
ritesh gurjar <gurjarritesh155@gmail.com>
2022-07-28 23:10:58 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const RINKBY_RPC_URL = process.env.RINKBY_RPC_URL || "https://eth-rinkeby";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+module.exports = {
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations:6,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+};
